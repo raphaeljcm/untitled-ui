5ab9bf3ce257ed297decd6a45ff87ab0681319ba
Raphael Marques <raphaeljcmarques@gmail.com>
2023-08-07 21:26:46 -0300

    wip-feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="flex h-full items-center justify-center">
+      Please, click on the sidebar links to navigate.
+    </div>
+  );
 }
 
 export default App;
