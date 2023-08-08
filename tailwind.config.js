0813bfb2c3bdb1421aad0ad99ded75dfa0e6db33
Raphael Marques <raphaeljcmarques@gmail.com>
2023-08-07 21:25:41 -0300

    chore: a few tailwind and prettier modifications

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
