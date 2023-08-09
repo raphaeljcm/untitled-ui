ea4acf767a4886648d1c2a26cf9030f59a04f6b4
Raphael Marques <raphaeljcmarques@gmail.com>
2023-08-08 21:57:07 -0300

    refactor: insert app_router constant to router component

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,15 +3,16 @@ import { Route, Routes } from 'react-router-dom';
 import { PublicLayout } from './layouts/PublicLayout';
 import { App } from './App';
 import { Loading } from '@/Loading';
+import { APP_ROUTES } from './consts/routes';
 const Home = lazy(() => import('./pages/Home'));
 
 export function Router() {
   return (
     <Suspense fallback={<Loading />}>
       <Routes>
-        <Route path="/" element={<PublicLayout />}>
+        <Route path={APP_ROUTES.APP} element={<PublicLayout />}>
           <Route
-            path="home"
+            path={APP_ROUTES.HOME}
             element={
               <Suspense fallback={<Loading />}>
                 <Home />
