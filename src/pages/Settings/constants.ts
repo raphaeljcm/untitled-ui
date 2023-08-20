1cf4ff888026fd02f10b31d3dfb94ac283306acd
Raphael Marques <raphaeljcmarques@gmail.com>
2023-08-20 17:09:56 -0300

    feat: avatar upload component

---
+++ src/pages/Settings/constants.ts
@@ -0,0 +1,42 @@
+export const SETTINGS_TABS = [
+  {
+    value: 'my-details',
+    title: 'My details',
+  },
+  {
+    value: 'profile',
+    title: 'Profile',
+  },
+  {
+    value: 'password',
+    title: 'Password',
+  },
+  {
+    value: 'team',
+    title: 'Team',
+  },
+  {
+    value: 'plan',
+    title: 'Plan',
+  },
+  {
+    value: 'billing',
+    title: 'Billing',
+  },
+  {
+    value: 'email',
+    title: 'Email',
+  },
+  {
+    value: 'notifications',
+    title: 'Notifications',
+  },
+  {
+    value: 'integrations',
+    title: 'Integrations',
+  },
+  {
+    value: 'api',
+    title: 'API',
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ export default {
         app: 'minmax(18rem, 20rem) 1fr',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
+      borderWidth: {
+        6: '6px',
+      },
+      colors: {
+        violet: {
+          25: '#FCFAFF',
+        },
+      },
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
       },
