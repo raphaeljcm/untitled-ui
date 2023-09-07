e081bda07b27712db99eac74a9891e8294620dc2
Raphael Marques <raphaeljcmarques@gmail.com>
2023-09-07 16:39:33 -0300

    style: index homepage darktheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export function App() {
   return (
     <div className="flex h-full flex-col items-center justify-center gap-2">
-      <p>Please, click on the sidebar links to navigate.</p>
+      <p className="dark:text-zinc-100">
+        Please, click on the sidebar links to navigate.
+      </p>
       <p className="text-sm text-violet-600">
         Hint: click on <span>Settings</span>, because it&apos;s the only one
         with content
